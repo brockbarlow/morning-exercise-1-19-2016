ae9ce67ec3278f2fc7c8dbea54dfeed55a3fce81
brockbarlow <brock.barlow@students.aie.edu.au>
2016-08-09 20:45:58 -0500

    removed zip folder

---
+++ bb 1-19-2016 Morning Exercise/1-19-2016 Morning Exercise/Assets/_Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
